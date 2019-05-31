d66ec3a25c9cf91408f82a345161fe0adca57854
juansa6201 <jcmare18@gmail.com>
2019-05-31 10:56:12 -0300

    mysql añadido

--- config/settings.py
+++ config/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cnq_backend',
+        'USER': 'mare',
+        'PASSWORD': os.environ.get("PASSWORD", ''),
+    	'HOST': '127.0.0.1', 
+    	'PORT': '3310',
     }
 }
 
